d8ea9a01d099b14cae440241c6ee5266270785d2
chernikit <n.a.chervotkin@gmail.com>
2024-05-14 18:47:37 +0500

    preparing for publication #2

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,9 @@ function scripts() {
 }
 
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', {
+    encoding: false
+  })
     .pipe(gulp.dest('dist/images'))
     .pipe(browserSync.reload({stream: true}));
 }
